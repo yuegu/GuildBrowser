63106b26742a722e5ccb30b8a739b33a2d9de4c3
Yue Gu <spoony.gu@gmail.com>
2012-11-12 14:04:16 +0800

    added test target

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Yue Gu on 11/12/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
