975b041d0b502cc3783dc054046efdc1f4122cf4
Yue Gu <spoony.gu@gmail.com>
2012-11-12 10:59:24 +0800

    Adding GuildBrowserLogicTests.

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Yue Gu on 11/12/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
